549ded361a3ba3937ec70ea4184e78fe82b349ae
zlei22 <56944979+zlei22@users.noreply.github.com>
2019-11-27 00:06:24 -0500

    Update models.py

--- map/models.py
+++ map/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext as _
 
-class Squirrels(models.Model):
+# Create your models here.
+class Squirrel(models.Model):
 
     # Latitude
     latitude = models.DecimalField(
@@ -10,7 +11,7 @@ class Squirrels(models.Model):
         decimal_places=15,
         )
     
-    # Longtitude
+    # Longitude
     longitude = models.DecimalField(
         help_text=_('Longitude'),
         max_digits=19,
@@ -22,4 +23,40 @@ class Squirrels(models.Model):
         help_text=_('Unique Squirrel ID'),
         max_length=255,
         primary_key=True,
-    )  
+    )
+    
+    # Shift
+    AM = 'AM'
+    PM = 'PM'
+
+    SHIFT_CHOICES = (
+        (AM, 'AM'),
+        (PM, 'PM'),
+    )
+
+    shift = models.CharField(
+        help_text=_('Shift'),
+        max_length=16,
+        choices=SHIFT_CHOICES,
+    )
+
+    # Date
+    date = models.DateField(
+        help_text=_('Date'),
+    )
+    
+    # Age
+    ADULT = 'Adult'
+    JUVENILE = 'Juvenile'
+
+    AGE_CHOICES = (
+        (ADULT, 'Adult'),
+        (JUVENILE, 'Juvenile'),
+    )
+
+    age = models.CharField(
+        help_text=_('Age'),
+        max_length=16,
+        choices=AGE_CHOICES,
+        blank=True,
+    )
