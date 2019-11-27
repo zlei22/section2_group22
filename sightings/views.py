43ccb608e827dc5a9adc25970f815e3340a6946c
高畅 <gaochang@dyn-160-39-138-35.dyn.columbia.edu>
2019-11-26 22:24:26 -0500

    correction

--- sightings/views.py
+++ sightings/views.py
@@ -12,3 +12,14 @@ def details(request,uid=None):
     sighting=Squirrel.objects.get(uid)
     context={'sighting':squirrel,}
     return render(request,"sightings/details.html",context)
+
+
+def add(request,Unique_Squirrel_Id=None):
+    if request.method=="POST":
+        print(request.POST)
+        data=request.POST
+        ret=Sightings.objects.create(Unique_Squirrel_Id='123',longitude=data['longitutde'])
+        if ret:
+            return HttpResponse('Your answer has been recorded successfully')
+        else:
+            return HttpResponse("Your answer hasn't been recorded")
