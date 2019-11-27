06eff729fbc8df4076da3358405b2bd99fba29a9
zlei22 <zlei_0422@outlook.com>
2019-11-27 03:58:51 +0000

    zl2847 commit

--- map/views.py
+++ map/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 
-from .models import Squirrels
+from .models import Squirrel
 
 # Create your views here.
 
 def index(request):
-    squirrels_tracker_latitude = Squirrels.objects.all().values_list('latitude')
-    squirrels_tracker_longitude = Squirrels.objects.all().values_list('longitude')
-    context = {'squirrels_latitude':squirrels_tracker_latitude,
-            'squirrels_longitude':squirrels_tracker_longitude}
+    latitude = Squirrel.objects.all().values_list('Latitude')
+    longitude = Squirrel.objects.all().values_list('Longitude')
+    context = {'Latitude':latitude,
+            'Longitude':longitude}
     return render(request,'map/squirrels_tracker_map.html',context)
