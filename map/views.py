0b039b5b150965aa0fc3bfcb8e0875e5135ef049
zlei22 <56944979+zlei22@users.noreply.github.com>
2019-11-27 00:31:33 -0500

    zl2847 commit

--- map/views.py
+++ map/views.py
@@ -8,8 +8,8 @@ from .models import Squirrel
 def index(request):
     squirrels_tracker_latitude = Squirrel.objects.all().values_list('Latitude')
     squirrels_tracker_longitude = Squirrel.objects.all().values_list('Longitude')
-    context = {'squirrels_latitude':squirrels_tracker_latitude,
-            'squirrels_longitude':squirrels_tracker_longitude}
+    context = {'Latitude':squirrels_tracker_latitude,
+            'Longitude':squirrels_tracker_longitude}
     return render(request,'map/squirrels_tracker_map.html',context)
 
 
