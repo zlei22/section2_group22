9bb713cc4a39e7948e2c5cb0427e8c6cd12c8055
zlei22 <zlei_0422@outlook.com>
2019-11-28 22:10:57 +0000

    Update sightings

--- sightings/views.py
+++ sightings/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
-from .models import Squirrel
-
+from django.http import HttpResponse
+from map.models import Squirrel
 
 def index(request):
-    squirrel=Squirrel.objects.all()
-    context={'sightings':squirrel,} 
-    return render(request,"sightings/index.html",context)
+    text = ''
+    sightings=Squirrel.objects.all()
+    for sighting in sightings:
+        text += sighting + '<This is a link.>'  + '\n'
+    return HttpResponse("This is a list of sightings.")
+   # return HttpResponse(text)
 
-
-def details(request,uid=None):
+def uid(request,uid=None):
     sighting=Squirrel.objects.get(uid)
-    context={'sighting':squirrel,}
-    return render(request,"sightings/details.html",context)
+    context={
+            'Sighting': sighting,
+    }
+    return render(request,"sightings/uid.html",context)
 
 def add(request,uid=None):
     if request.method=="POST":
         print(request.POST)
         data=request.POST
+    context={
+            'Sighting': sightings,
+    }
+    return render(request,"sightings/add.html",context)
+
+def stats(request):
+    sightings=Squirrel.objects.all()
+    context={
+            'Sighting': sightings,
+    }
+    return render(request,"sightings/stats.html",context)
