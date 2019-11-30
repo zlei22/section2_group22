9ba38f2b4c4434976ec90c33f6b473965c779910
zlei22 <zlei_0422@outlook.com>
2019-11-30 16:43:41 +0000

    Update sightings/views.py

--- sightings/views.py
+++ sightings/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Sighting
-from map.models import Squirrel
 
 def index(request):
-<<<<<<< HEAD
     sightings=Sighting.objects.all()
     context={
         'sightings': sightings,
     }
     return render(request,"sightings/index.html",context)
-    
->>>>>>> 8ba5d5d05ca71767436e540899f4ac87eae0f7bf
 
 def uid(request,uid):
     sighting=Sighting.objects.get(uid=uid)
@@ -30,7 +26,7 @@ def add(request,uid=None):
     return render(request,"sightings/add.html",context)
 
 def delete(request,uid=None):
-    sighting=squirrels.objects.get(uid=uid)
+    sighting=Sighting.objects.get(uid=uid)
     if request.method=="POST":
         sighting.delete()
         return redirect('sightings:index')
@@ -38,7 +34,7 @@ def delete(request,uid=None):
         context={
             'Sighting': sightings,
     }
-        return render(request,"sightings/delete.html",context)
+    return render(request,"sightings/delete.html",context)
 
 def stats(request):
     sightings=Sighting.objects.all()
