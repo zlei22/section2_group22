7ad93a70bf8fcb66211ef96478350b86fee98098
zlei22 <zlei_0422@outlook.com>
2019-11-30 17:48:14 +0000

    Update sightings/add.html

--- sightings/views.py
+++ sightings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Sighting
 
+
 def index(request):
     sightings=Sighting.objects.all()
     context={
@@ -9,36 +10,63 @@ def index(request):
     }
     return render(request,"sightings/index.html",context)
 
+
 def uid(request,uid):
     sighting=Sighting.objects.get(uid=uid)
-    context={
-        'sighting': sighting,
-    }
-    return render(request,"sightings/uid.html",context)
-
-def add(request,uid=None):
     if request.method=="POST":
-        print(request.POST)
-        data=request.POST
-    context={
-        'sighting': sightings,
-    }
-    return render(request,"sightings/add.html",context)
-
-def delete(request,uid=None):
-    sighting=Sighting.objects.get(uid=uid)
-    if request.method=="POST":
+        sighting.shift=request.POST['sighting.shift']
+        sighting.save()
+        return redirect('sightings:uid')
+    elif request.method=="DELETE":
         sighting.delete()
         return redirect('sightings:index')
     else:
         context={
-            'Sighting': sightings,
-    }
-    return render(request,"sightings/delete.html",context)
+            'sighting': sighting,
+        }
+        return render(request,"sightings/uid.html",context)
+
+
+def add(request):
+    if request.method=="POST":
+        uid = request.POST.get('uid')
+        latitude = request.POST.get('latitude')
+        longitude = request.POST.get('longitude')
+        shift = request.POST.get('shift')
+        date = request.POST.get('date')
+        age = request.POST.get('age')
+        color = request.POST.get('color')
+        loc = request.POST.get('loc')
+        specific_loc = request.POST.get('specific_loc')
+        running = request.POST.get('running')
+        chasing = request.POST.get('chasing')
+        climbing = request.POST.get('climbing')
+        eating = request.POST.get('eating')
+        foraging = request.POST.get('foraging')
+        other_act = request.POST.get('other_act')
+        kuks = request.POST.get('kuks')
+        quaas = request.POST.get('quaas')
+        moans = request.POST.get('moans')
+        tail_flags = request.POST.get('tail_flags')
+        tail_twitches = request.POST.get('tail_twitches')
+        approaches = request.POST.get('approaches')
+        indifferent = request.POST.get('indifferent')
+        runs_from = request.POST.get('runs_from')
+
+        sighting=Sighting.objects.create(uid=uid, latitude=latitude, longitude=longitude,
+                shift=shift, date=date, age=age, color=color, loc=loc, specific_loc=specific_loc,
+                running=running, chasing=chasing, climbing=climbing, eating=eating, foraging=foraging,
+                other_act=other_act, kuks=kuks, quaas=quaas, moans=moans, tail_flags=tail_flags,
+                tail_twitches=tail_twitches, approaches=approaches, indifferent=indifferent,
+                runs_from=runs_from)
+        sighting.save()
+        return redirect('sightings:index')
+    return render(request,"sightings/add.html",context)
+
 
 def stats(request):
     sightings=Sighting.objects.all()
     context={
-        'sighting': sightings,
+        'sightings': sightings,
     }
     return render(request,"sightings/stats.html",context)
