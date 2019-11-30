8ba5d5d05ca71767436e540899f4ac87eae0f7bf
高畅 <gaochang@gcs-macbook-pro.home>
2019-11-30 00:19:17 -0500

    add delete.html

--- sightings/views.py
+++ sightings/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Sighting
+from map.models import Squirrel
 
 def index(request):
-    text = ''
-    sightings=Sighting.objects.all()
-    for sighting in sightings:
-        text += "Squirrel ID: %s<br>" %(sighting.uid)
+    #text = ''
+    #sightings=Sighting.objects.all()
+    #for sighting in sightings:
+     #   text += "Squirrel ID: %s<br>" %(sighting.uid)
     # return HttpResponse("This is a list of sightings.")
-    return HttpResponse(text)
+    #return HttpResponse(text)
+    sightings=Squirrel.objects.all()
+    context={'sightings':sightings,} 
+    return render(request,"sightings/index.html",context)
+    
 
-def uid(request,uid=None):
-    sighting=Sighting.objects.get(uid)
+def uid(request,uid):
+    sighting=Sighting.objects.get(uid=uid)
     context={
             'Sighting': sighting,
     }
@@ -25,6 +30,17 @@ def add(request,uid=None):
             'Sighting': sightings,
     }
     return render(request,"sightings/add.html",context)
+
+def delete(request,uid=None):
+    sighting=squirrels.objects.get(uid=uid)
+    if request.method=="POST":
+        sighting.delete()
+        return redirect('sightings:index')
+    else:
+        context={
+            'Sighting': sightings,
+    }
+        return render(request,"sightings/delete.html",context)
 
 def stats(request):
     sightings=Sighting.objects.all()
