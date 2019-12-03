19035780795fb43c695a2ebc622dcc0482007f8c
Chang Gao <cg3177@columbia.edu>
2019-12-03 17:45:10 +0000

    add  stats html

--- map/views.py
+++ map/views.py
@@ -7,10 +7,14 @@ from sightings.models import Sighting
 def index(request):
     squirrels_x = Sighting.objects.all().values_list('longitude')
     squirrels_y = Sighting.objects.all().values_list('latitude')
+    
+
     context = {'longitude':squirrels_x,
             'latitude':squirrels_y}
     #template = loader.get_template('map.html')
     #return HttpResponse(template.render(context, request))
     return render(request,'map/squirrel_tracker_map.html',context)
 
-# test
+
+
+
