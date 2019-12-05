eb642a87065cc697e5a14f01776f62cb41f3a3a1
zlei22 <zlei_0422@outlook.com>
2019-12-05 02:26:36 +0000

    Update views.py

--- sightings/urls.py
+++ sightings/urls.py
@@ -8,14 +8,16 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # Update and Delete: /sightings/<uid>
-    # path('<str:uid>', views.uid, name='update'),
-    url(r'^<str:uid>$', views.uid),
+    path('<str:uid>', views.uid, name='details'),
+    # url(r'^<str:uid>$', views.uid),
+
+    path('<str:uid>/edit', views.edit, name='edit'),
 
     # Add New Sightings: /sightings/add
-    # path('add', views.add, name='add'),
-    url(r'^add$', views.add),
+    path('add', views.add, name='add'),
+    # url(r'^add$', views.add),
 
     # Check Up Statistics: /sightings/stats
-    # path('stats', views.stats, name='stats'),
-    url(r'^stats$', views.stats),
+    path('stats', views.stats, name='stats'),
+    # url(r'^stats$', views.stats),
  ]
