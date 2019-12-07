2a3536059775e006caefc6583aaf76d6ca011c01
Chang Gao <cg3177@columbia.edu>
2019-12-07 16:04:52 +0000

    export data

--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -1,1 +1,25 @@
 # This is an export command.
+
+from django.core.management.base import BaseCommand
+from sightings.models import Sighting
+import csv
+import sys
+
+class Command(BaseCommand):
+    help = 'export data'
+
+    def add_arguments(self,parser):
+        parser.add_argument('csv_file')
+
+    def handle(self,*args,**options):
+
+        with open(options['csv_file'])as fp:
+            export_data = csv.writer(fp,delimiter='\t')
+            fields = [f.name for f in Sighting._meta.fields]
+            export_data.writerow(fields)
+            for line in Sightings.objects.all():
+                export_data.writerow([getattr(line,i) for i in fields])
+
+
+
+
